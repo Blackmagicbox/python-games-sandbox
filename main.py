c5285aac03bfae0118309238f604c8e956ff2856
Rafael Santos <15151000+Blackmagicbox@users.noreply.github.com>
2024-03-04 12:35:48 +0100

    Feat lists Comprehensions

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+import math
+
 print("Hello, World!")
+y = lambda x: math.sqrt(x)
+
+print(y(4))
+
+chess_board = [[f'{letter}{num}' for num in range(1,9)] for letter in 'ABCDEFGH'[::-1]]
+for row in chess_board:
+    print(row)
