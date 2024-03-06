ec5829dd41e1f5eed17e7ee8ade0004ca8ef13fe
Rafael Santos <15151000+Blackmagicbox@users.noreply.github.com>
2024-03-06 23:13:42 +0100

    Test

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
-import math
+# import math
+#
+# print("Hello, World!")
+# y = lambda x: math.sqrt(x)
+#
+# print(y(4))
+#
+# chess_board = [[f'{letter}{num}' for num in range(1,9)] for letter in 'ABCDEFGH'[::-1]]
+# for row in chess_board:
+#     print(row)
 
-print("Hello, World!")
-y = lambda x: math.sqrt(x)
-
-print(y(4))
-
-chess_board = [[f'{letter}{num}' for num in range(1,9)] for letter in 'ABCDEFGH'[::-1]]
-for row in chess_board:
-    print(row)
+rows = list('ABCDEFGH')
+cols = list(range(1,9))
+board_positions = list(zip(rows, cols))
+print(board_positions)
+sorted_by_col = sorted(board_positions, key=lambda square: -1 * square[1])
+print(sorted_by_col)
