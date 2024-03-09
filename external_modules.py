9e7b31dbb8b487572de251353cb2d8b62750a060
Rafael Santos <15151000+Blackmagicbox@users.noreply.github.com>
2024-03-09 01:38:52 +0100

    FEAT Pyplot example [DRAFT]

---
+++ external_modules.py
@@ -0,0 +1,11 @@
+import pyautogui
+from time import sleep
+from matplotlib import pyplot as plt
+
+plt.plot([1, 2, 3, 4, 10, 0, 100])
+plt.show()
+
+# Hello Auto writer!
+
+# sleep(4)
+# pyautogui.write("# Hello Auto writer! \n")
